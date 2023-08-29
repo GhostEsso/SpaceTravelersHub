8ec8194d94b4c5647615e65b7b4f3916f4fcc180
GhostEsso <lilipitaham@gmail.com>
2023-08-29 09:48:15 +0000

    Rocketslice upadted with reservation

--- src/redux/rockets/RocketsSlice.js
+++ src/redux/rockets/RocketsSlice.js
@@ -1,34 +1,40 @@
+// Import the necessary dependencies
 import { createSlice } from '@reduxjs/toolkit';
 import { FetchData } from './Api';
 
+// Initial Redux slice state
 const initialState = {
-  rockets: [],
-  status: true,
-  error: null,
+  rockets: [], // Empty array to store rocket information
+  status: true, // Indicator to track if the search is in progress
+  error: null, // Store any errors
 };
 
-// Now I create a place to store rocket information
+// Create the "Rocketslice" slice
 const Rocketslice = createSlice({
-    name: 'Rockets',
-    initialState,
-    reducers: {
-        reserveRocket: (state, { payload }) => {
-          const rockets = state.rockets.map((rocket) => {
-            if (rocket.id === payload) return { ...rocket, reserved: !rocket.reserved };
-            return rocket;
-          });
-        },
-      },
-    extraReducers(builder) {
-      builder
-        // When I'm done looking up the rocket info and it's alright
-        .addCase(FetchData.fulfilled, (state, { payload }) => {
-          state.status = false; //  searching finished
-          
-          state.rockets = data;
-        });
+  name: 'Rockets', // Slice name
+  initialState, // Initial state previously defined
+  reducers: {
+    // Action to reserve or cancel the reservation of a rocket
+    reserveRocket: (state, { payload }) => {
+    // Traverse the rocket array to update the reservation  
+      const rockets = state.rockets.map((rocket) => {
+        if (rocket.id === payload) return { ...rocket, reserved: !rocket.reserved };
+        return rocket;
+      });
+      return { ...state, rockets }; // Return the new state with the updated rocket array
     },
+  },
+  extraReducers(builder) {
+    // Handling asynchronous actions such as FetchData
+    builder
+      .addCase(FetchData.fulfilled, (state, action) => ({
+        ...state,
+        rockets: action.payload, // Update the rocket data in the state
+        status: false, // Indicates that the search is finished
+      }));
+  },
 });
 
-export const { reserveRocket } = Rocketslice.actions;
-export default Rocketslice.reducer;
+// Export created actions and reducer
+export const { reserveRocket } = Rocketslice.actions; // Action to reserve a rocket
+export default Rocketslice.reducer; // Global reducer to manage the state of the rockets
