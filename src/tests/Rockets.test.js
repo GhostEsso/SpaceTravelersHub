70d767f4a54541d13cc0e22eb825057b09bbf5a6
Errytagedesign <errytage@gmail.com>
2023-08-31 18:38:02 +0100

    Fix Testing fail

--- src/tests/Rockets.test.js
+++ src/tests/Rockets.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../../redux/store';
-import Rockets from '../../components/rockets/Rockets';
+import store from '../redux/store';
+import Rockets from '../components/rockets/Rockets';
 
 test('Check if dragon component is rendered correctly', () => {
   const tree = render(
